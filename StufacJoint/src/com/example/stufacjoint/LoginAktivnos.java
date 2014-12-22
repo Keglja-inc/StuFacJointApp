7eeff2c5dfd4de91a8f2a67d485b6e3b677122e4
deslunjsk <denodeno6@gmail.com>
2014-12-22 17:03:38 +0100

    Nakon prve faze aplikacije, u ovoj verziji dodana je mogućnost logina putem facebook profila. Nisam siguran da li će s eu ovom comitu povući gore i facebooksdk mapa koja je potrebna kako bismo koristili libary za logiranje putem facebooka. Stoga ako nije moguće skinuti morate skinuti prvo facebook sdk zatim ga importati u workspace projekta, povezati ga sa projektom stufacjoint kao vanjski libari i spremni ste za testtiranje aplikacije.

--- StufacJoint/src/com/example/stufacjoint/LoginAktivnos.java
+++ StufacJoint/src/com/example/stufacjoint/LoginAktivnos.java
@@ -1,10 +1,14 @@
 package com.example.stufacjoint;
 
+import java.io.IOException;
+import java.net.MalformedURLException;
+
 import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.content.Intent;
 import android.graphics.Color;
 import android.os.Bundle;
+import android.preference.PreferenceManager.OnActivityResultListener;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -15,15 +19,24 @@ import android.widget.ImageButton;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.facebook.android.DialogError;
+import com.facebook.android.Facebook;
+import com.facebook.android.Facebook.DialogListener;
+import com.facebook.android.FacebookError;
+
 @SuppressLint("ShowToast")
 public class LoginAktivnos extends Activity {
 	
+   
+    Facebook fb;
+    
 	private EditText  ime=null;
 	   private EditText  lozinka=null;
 	   private TextView attempts;
-	   private Button login;
+	  private Button login;
 	   int counter = 3;
 
+	@SuppressWarnings("deprecation")
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
@@ -34,18 +47,75 @@ public class LoginAktivnos extends Activity {
 	      attempts = (TextView)findViewById(R.id.textView5);
 	      attempts.setText(Integer.toString(counter));
 	      login = (Button)findViewById(R.id.logingumb);
+	      
+	      String app_id=getString(R.string.facebook_app_id);
+	  	  fb = new Facebook(app_id);
+	      
+	      
 	      ImageButton facebook=(ImageButton)findViewById(R.id.facebookgumb);
-		  	facebook.setOnClickListener(new OnClickListener() {
+		  	
+	      facebook.setOnClickListener(new OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
 				
-				@SuppressLint("ShowToast")
-				@Override
-				public void onClick(View arg0) {
-					Toast.makeText(getApplicationContext(), "Ovaj gumb trenutno nije u funkciji", Toast.LENGTH_SHORT).show();
+				
+				if(fb.isSessionValid()){
+					
+					
+					try{
+						
+					fb.logout(getApplicationContext());
+				
+					}
+					catch(MalformedURLException e){
+						e.printStackTrace();
+					
+					
+				} catch (IOException e) {
+						e.printStackTrace();
+					}
 					
 				}
-			});
+				else{
+					
+					fb.authorize(LoginAktivnos.this,new String[] {"email"} , new DialogListener() {
+						
+						@Override
+						public void onFacebookError(FacebookError e) {
+							Toast.makeText(LoginAktivnos.this, "Nemate pristup internetu", Toast.LENGTH_SHORT).show();
+							
+						}
+						
+						@Override
+						public void onError(DialogError e) {
+							Toast.makeText(LoginAktivnos.this, "U ovom trenutku nije moguæe pristupiti aplikaciji", Toast.LENGTH_SHORT).show();
+							
+						}
+						
+						@Override
+						public void onComplete(Bundle values) {
+							Intent i=new Intent(getApplicationContext(), Glavna_aktivnost.class);
+							startActivity(i);
+							
+						}
+						
+						@Override
+						public void onCancel() {
+							Toast.makeText(LoginAktivnos.this, "Prekinuti login", Toast.LENGTH_SHORT).show();
+							
+						}
+						
+					});
+					
+				}
+			
+		}
+		
+		});
 		
 		
+
 	}
 	
 	public void login(View view){
@@ -59,7 +129,8 @@ public class LoginAktivnos extends Activity {
 				startActivity(i);
 				
 			}
-		});
+	      }
+		);
 	   }	
 	   else{
 	      Toast.makeText(getApplicationContext(), "Pokušajte ponovno",
